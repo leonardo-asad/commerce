557e46e9ccfa4ca12622001680627296652d52c3
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-03-28 20:33:08 -0300

    Fixed __str__ method

--- auctions/models.py
+++ auctions/models.py
@@ -1,13 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from models import Model
-
-
 
 class User(AbstractUser):
     pass
 
-class Listing(Model):
+class Listing(models.Model):
     date = models.DateField()
     title = models.CharField(max_length=64)
     description = models.TextField()
@@ -15,14 +12,23 @@ class Listing(Model):
     category = models.CharField(max_length=64)
     listed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="publications")
 
-class Bid(Model):
+    def __str__(self):
+        return f"Listing title: {self.title}. Created on {self.date}. Listed by: {listed_by}"
+
+class Bid(models.Model):
     date = models.DateField()
     bid = models.PositiveIntegerField()
     bid_author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bids")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_bids")
 
-class Comment(Model):
+    def __str__(self):
+        return f"Bid made by: {self.bid_author}. Created on {self.date}. On listing: {self.listed_by}"
+
+class Comment(models.Model):
     comment = models.TextField()
     date = models.DateField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing_comments")
+
+    def __str__(self):
+        return f"Comment made by: {self.author}. Created on {self.date}. On listing: {self.listing}"
